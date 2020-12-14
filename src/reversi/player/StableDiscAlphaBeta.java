7c863f290fdfd8b92f47118e72d94db33e69740d
Freakness109 <freakness109@gmail.com>
2020-12-14 22:03:39 +0100

    Add StableDiscAlphaBeta

---
+++ src/reversi/player/StableDiscAlphaBeta.java
@@ -0,0 +1,27 @@
+package reversi.player;
+
+import reversi.Coordinates;
+import reversi.GameBoard;
+import reversi.ReversiPlayer;
+import reversi.deciders.AlphaBetaDecider;
+import reversi.raters.StableDiscRater;
+import reversi.utils.IDecideMove;
+import reversi.utils.IRateBoard;
+
+public class StableDiscAlphaBeta implements ReversiPlayer {
+    private IDecideMove decideMove;
+    @Override
+    public void initialize(int i, long l) {
+        decideMove = new AlphaBetaDecider();
+        IRateBoard rater = new StableDiscRater();
+        rater.setPlayer(i);
+        decideMove.setRater(rater);
+        decideMove.setTimeout(l);
+        decideMove.setPlayer(i);
+    }
+
+    @Override
+    public Coordinates nextMove(GameBoard gameBoard) {
+        return decideMove.nextMove(gameBoard);
+    }
+}
